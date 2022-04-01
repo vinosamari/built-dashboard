7287241d7732bcf2392e9f1a0bdd7cc16a0ee907
Vino Samari <iamvinomusic@gmail.com>
2022-04-01 15:18:47 +0000

    first commit

---
+++ components/ComingSoon.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Coming Soon!</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <coming-soon></coming-soon>
+</template>
+
+<script>
+import ComingSoon from "~/components/ComingSoon.vue";
+export default {};
+</script>
+
+ComingSoon

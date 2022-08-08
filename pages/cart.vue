1904b93aa4cd27700c23309e648ca49a392cd08d
vinosamari <iamvinomusic@gmail.com>
2022-08-08 18:42:43 +0000

    added order table

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,5 +1,19 @@
 <template>
-  <main>YOUR CART IS EMPTY</main>
+  <main>
+    <div v-if="this.$store.state.cart.length === 0">your cart is empty</div>
+    <div v-else>
+      <order-table :checkedOut="checkedOut"></order-table>
+      <div class="checkoutSection">
+        <div class="buttons">
+          <button @click="goToStore" v-show="!checkedOut">
+            Continue shopping
+          </button>
+          <button @click="proceedToCheckout">Proceed to checkout</button>
+          <div class="customerDetailsSection"></div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -11,13 +25,16 @@ export default {
       customerPhone: "",
       customerAddress: "",
       checkedOut: false,
-      // cartCount: this.$store.state.cartCount,
-      // orderTotal: this.$store.state.orderTotal,
-      // orders: this.$store.state.cart,
       delivery: false,
     };
   },
   methods: {
+    proceedToCheckout() {
+      this.checkedOut = true;
+    },
+    goToStore() {
+      this.$router.push("/store");
+    },
     generateReference() {
       let date = new Date();
       return date.getTime().toString();
@@ -59,6 +76,12 @@ export default {
 
 <style scoped>
 main {
-  @apply min-h-screen;
+  @apply min-h-screen font-customOne;
+}
+.buttons {
+  @apply flex flex-col items-center justify-center;
+}
+.buttons button {
+  @apply bg-black font-bold uppercase tracking-wide text-sm px-3 py-1 rounded-md my-3 text-white;
 }
 </style>

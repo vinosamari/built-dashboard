7b3b5bf0cf18df112fe655f5c6932bca99931501
Vino Samari <iamvinomusic@gmail.com>
2023-02-08 11:31:09 +0000

    Added bars

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,9 +22,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    // { src: "~/plugins/vuex-persist", ssr: false },
-  ],
+  plugins: [{ src: "~/plugins/vue-bars", mode: "client" }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
